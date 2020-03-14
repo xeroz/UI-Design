bc99066c6d57204d70cded04b58ab247eed4fd22
juan <sdf@sdf.com>
2020-03-14 11:50:14 -0500

    scroll

--- DesignCode/HomeView.swift
+++ DesignCode/HomeView.swift
@@ -9,70 +9,68 @@
 import SwiftUI
 
 struct HomeView: View {
-    @State var showProfile = false
-    @State var viewState = CGSize.zero
-
+    @Binding var showProfile: Bool
+    
     var body: some View {
-        ZStack {
-            Color(#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1))
-                .edgesIgnoringSafeArea(.all)
-            
-            VStack{
-                HStack {
-                    Text("Watching")
-                        .font(.system(size: 28, weight: .bold))
-                    
-                    Spacer()
+        VStack{
+            HStack {
+                Text("Watching")
+                    .font(.system(size: 28, weight: .bold))
 
-                    Button(action: { self.showProfile.toggle() }){
-                        Image("Avatar")
-                            .renderingMode(.original)
-                            .resizable()
-                            .frame(width: 36, height: 36)
-                            .clipShape(Circle())
-                    }
-                }
-                .padding(.horizontal)
-                .padding(.top, 30)
-                
                 Spacer()
+
+                AvatarView(showProfile: $showProfile)
             }
-            .padding(.top, 44)
-            .background(Color.white)
-            .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
-            .shadow(color: Color.black.opacity(0.2), radius: 20, x: 0, y: 20)
-            .rotation3DEffect(Angle(degrees: showProfile ? Double(viewState.height / 10) - 10 : 0), axis: (x: 10, y: 0, z: 0))
-            .offset(y: showProfile ? -450 : 0)
-            .scaleEffect(showProfile ? 0.9 : 1)
-            .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
-            .edgesIgnoringSafeArea(.all)
-            
-            MenuView()
-                .background(Color.black.opacity(0.001))
-                .offset(y: showProfile ? 0 : 1000)
-                .offset(y: viewState.height)
-                .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
-                .onTapGesture {
-                    self.showProfile.toggle()
+            .padding(.horizontal)
+            .padding(.leading, 14)
+            .padding(.top, 30)
+
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack {
+                    ForEach(0 ..< 3) { item in
+                        SectionView()
+                    }
                 }
-                .gesture(
-                    DragGesture().onChanged { value in
-                        self.viewState = value.translation
-                    }
-                    .onEnded { value in
-                        if self.viewState.height > 50 {
-                            self.showProfile = false
-                        }
-                        self.viewState = .zero
-                    }
-                )
-            
+                .padding(30)
+                .padding(.horizontal, 30)
+            }
+
+            Spacer()
         }
     }
 }
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
-        HomeView()
+        HomeView(showProfile: .constant(false))
+    }
+}
+
+struct SectionView: View {
+    var body: some View {
+        VStack {
+            HStack(alignment: .top) {
+                Text("Prototype designs in SwiftUI")
+                    .font(.system(size: 24, weight: .bold))
+                    .frame(width: 160, alignment: .leading)
+                    .foregroundColor(.white)
+                Spacer()
+                Image("Logo1")
+            }
+            
+            Text("18 Section".uppercased())
+                .frame(maxWidth: 160, alignment: .leading)
+            
+            Image("Card1")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 210)
+        }
+        .padding(.top, 20)
+        .padding(.horizontal, 20)
+        .frame(width: 275, height: 275)
+        .background(Color("card1"))
+        .cornerRadius(30)
+        .shadow(color: Color("card1").opacity(0.3), radius: 20, x: 0, y:20)
     }
 }
