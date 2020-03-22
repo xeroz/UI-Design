764bf251591f6335df581fbf0d039f9528b9732a
juan <sdf@sdf.com>
2020-03-22 11:58:51 -0500

    tabitem

---
+++ DesignCode/TopBar.swift
@@ -0,0 +1,36 @@
+//
+//  TopBar.swift
+//  DesignCode
+//
+//  Created by juan on 3/22/20.
+//  Copyright © 2020 juan. All rights reserved.
+//
+
+import SwiftUI
+
+struct TopBar: View {
+    var body: some View {
+        TabView {
+            Home().tabItem {
+                Image(systemName: "play.circle.fill")
+                Text("Home")
+            }
+            
+            ContentView().tabItem {
+                Image(systemName: "rectangle.stack.fill")
+                Text("Certificates")
+            }
+        }
+        .edgesIgnoringSafeArea(.top)
+    }
+}
+
+struct TopBar_Previews: PreviewProvider {
+    static var previews: some View {
+//        TopBar()
+        Group {
+            TopBar().previewDevice("iPhone 8")
+            TopBar().previewDevice("iPhone 11 ProMax")
+        }
+    }
+}
