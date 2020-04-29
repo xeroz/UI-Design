8e7cc69a586c44e22a4ffcd2ec48b834cb2284a8
juan <sdf@sdf.com>
2020-04-28 20:16:36 -0500

    fonts

--- DesignCode/HomeView.swift
+++ DesignCode/HomeView.swift
@@ -16,7 +16,8 @@ struct HomeView: View {
         VStack{
             HStack {
                 Text("Watching")
-                    .font(.system(size: 28, weight: .bold))
+//                    .font(.system(size: 28, weight: .bold))
+                    .modifier(CustomFontModifier(size: 28))
 
                 Spacer()
 
@@ -41,6 +42,12 @@ struct HomeView: View {
             .padding(.top, 30)
 
             ScrollView(.horizontal, showsIndicators: false) {
+                WatchRingsView()
+                    .padding(.horizontal, 30)
+                    .padding(.bottom, 30)
+            }
+            
+            ScrollView(.horizontal, showsIndicators: false) {
                 HStack(spacing: 20) {
                     ForEach(sectionData) { item in
                         GeometryReader { geometry in
@@ -53,10 +60,10 @@ struct HomeView: View {
                         .frame(width: 275, height: 275)
                     }
                 }
-                .padding(30)
-                .padding(.horizontal, 30)
+                .padding(.horizontal)
+                .padding(.leading, 14)
+                .padding(.top, 30)
             }
-
             Spacer()
         }
     }
@@ -128,3 +135,51 @@ let sectionData = [
             color: Color(#colorLiteral(red: 0.8549019694, green: 0.250980407, blue: 0.4784313738, alpha: 1))
     )
 ]
+
+struct WatchRingsView: View {
+    var body: some View {
+        HStack(spacing: 30.0) {
+            HStack(spacing: 12.0) {
+                RingView(
+                    color1: #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1),
+                    color2: #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1),
+                    width: 44, height: 44, percent: 68, show: .constant(true))
+                
+                VStack(alignment: .leading, spacing: 4.0) {
+                    Text("6 minutes left")
+                        .bold()
+                        .modifier(FontModifier(style: .subheadline))
+                    Text("Watched 10 mins today")
+                        .modifier(FontModifier(style: .caption))
+                }
+                .modifier(FontModifier())
+            }
+            .padding(8)
+            .background(Color.white)
+            .cornerRadius(20)
+            .modifier(ShadowModifier())
+            
+            HStack(spacing: 12.0) {
+                RingView(
+                    color1: #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1),
+                    color2: #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1),
+                    width: 32, height: 32, percent: 54, show: .constant(true))
+            }
+            .padding(8)
+            .background(Color.white)
+            .cornerRadius(20)
+            .modifier(ShadowModifier())
+            
+            HStack(spacing: 12.0) {
+                RingView(
+                    color1: #colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1),
+                    color2: #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1),
+                    width: 32, height: 32, percent: 32, show: .constant(true))
+            }
+            .padding(8)
+            .background(Color.white)
+            .cornerRadius(20)
+            .modifier(ShadowModifier())
+        }
+    }
+}
--- DesignCode/RingView.swift
+++ DesignCode/RingView.swift
@@ -14,6 +14,7 @@ struct RingView: View {
     var width: CGFloat = 300
     var height: CGFloat = 300
     var percent: CGFloat = 88
+    @Binding var show: Bool
     
     var body: some View {
         let multiplier = width / 44
@@ -25,7 +26,7 @@ struct RingView: View {
                 .frame(width: width, height: height)
 
             Circle()
-                .trim(from: progress, to: 1)
+                .trim(from: show ? progress : 1, to: 1)
                 .stroke(
                     LinearGradient(
                         gradient: Gradient(
@@ -56,14 +57,16 @@ struct RingView: View {
             Text("\(Int(percent))%")
                 .font(.system(size: 14 * multiplier))
                 .fontWeight(.bold)
+                .onTapGesture {
+                    self.show.toggle()
+                }
         }
     
-//        Color(#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1))
     }
 }
 
 struct RingView_Previews: PreviewProvider {
     static var previews: some View {
-        RingView()
+        RingView(show: .constant(true))
     }
 }
