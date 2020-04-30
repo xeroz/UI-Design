637f7cd9985508e74732887fe9f3a9237aea1f7f
juan <sdf@sdf.com>
2020-04-30 12:12:48 -0500

    z index

--- DesignCode/CourseList.swift
+++ DesignCode/CourseList.swift
@@ -12,28 +12,39 @@ struct CourseList: View {
     @State var show = false
     @State var show2 = false
     @State var courses = courseData
+    @State var active = false
 
     var body: some View {
-        ScrollView {
-            VStack(spacing: 30) {
-                Text("Courses")
-                .font(.system(.largeTitle))
-                .fontWeight(.bold)
-                .frame(maxWidth: .infinity, alignment: .leading)
-                    .padding(.leading, 30)
-                .padding(.top, 30)
-                
-                ForEach(courses.indices, id: \.self) { index in
-                    GeometryReader{geometry in
-                        CourseView(show: self.$courses[index].show, course: self.courses[index])
-                            .offset(y: self.courses[index].show ? -geometry.frame(in: .global).minY : 0)
+        ZStack {
+            Color.black.opacity(active ? 0.5 : 0)
+                .animation(.linear)
+                .edgesIgnoringSafeArea(.all)
+            
+            ScrollView {
+                VStack(spacing: 30) {
+                    Text("Courses")
+                        .font(.system(.largeTitle))
+                        .fontWeight(.bold)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding(.leading, 30)
+                        .padding(.top, 30)
+                        .blur(radius: active ? 20 : 0)
+                    
+                    ForEach(courses.indices, id: \.self) { index in
+                        GeometryReader{geometry in
+                            CourseView(show: self.$courses[index].show, course: self.courses[index], active: self.$active)
+                                .offset(y: self.courses[index].show ? -geometry.frame(in: .global).minY : 0)
+                        }
+                        .frame(height: 280)
+                        .frame(maxWidth: self.courses[index].show ? .infinity : screen.width - 60)
+                        .zIndex(self.courses[index].show ? 1 : 0)
                     }
-                    .frame(height: 280)
-                    .frame(maxWidth: self.courses[index].show ? .infinity : screen.width - 60)
                 }
+                .frame(width: screen.width)
+                .animation(.spring(response: 0.5, dampingFraction: 0.7, blendDuration: 0))
             }
-            .frame(width: screen.width)
-            .animation(.spring(response: 0.5, dampingFraction: 0.7, blendDuration: 0))
+            .statusBar(hidden: active ? true : false)
+            .animation(.linear)
         }
     }
 }
@@ -47,6 +58,7 @@ struct CourseList_Previews: PreviewProvider {
 struct CourseView: View {
     @Binding var show: Bool
     var course: Course
+    @Binding var active: Bool
     
     var body: some View {
         ZStack(alignment: .top) {
@@ -77,7 +89,9 @@ struct CourseView: View {
                         Text(course.subtitle)
                             .foregroundColor(Color.white.opacity(0.7))
                     }
+                    
                     Spacer()
+                    
                     ZStack {
                         Image(uiImage: course.logo)
                             .opacity(show ? 0 : 1)
@@ -111,6 +125,7 @@ struct CourseView: View {
                 radius: 20, x: 0, y: 20)
             .onTapGesture {
                 self.show.toggle()
+                self.active.toggle()
             }
         }
         .frame(height: show ? screen.height : 280)
